3d52d3c94796debb4e1d15ea27e04b4ea551c33b
rolando-cadiz <143847732+rolando-cadiz@users.noreply.github.com>
2026-02-03 19:40:50 -1000

    metadata quick update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Features from '@/components/landing/Features';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Pantry Pals',
-  description: 'Manage your pantry, reduce food waste, and cook smarter.',
+  title: 'Shelf Awareness',
+  description: 'Manage your pantry shelf, reduce food waste, and cook smarter.',
   icons: '/favicon.ico',
 };
 
